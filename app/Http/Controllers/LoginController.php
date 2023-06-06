27415a5191ddf009f1c282974c769339e86f8618
EventHorizon8 <t.babarina@gmail.com>
2023-06-07 00:22:43 +0200

    login user and load authorised

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Nette\Schema\ValidationException;
+
+class LoginController extends Controller
+{
+    /**
+     * Handle an authentication attempt.
+     */
+    public function authoriseUser(Request $request): JsonResponse
+    {
+        $credentials = $request->validate([
+            'email' => ['required', 'email'],
+            'password' => ['required'],
+            'device_name' => ['required'],
+        ]);
+
+        $user = User::where('email', $request->email)->first();
+
+        if (! $user || ! Hash::check($request->password, $user->password)) {
+            response()->json(['error' => 'Access denied'], 400);
+        }
+
+        return response()->json(['token' => $user->createToken($request->device_name)->plainTextToken], 200);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+Route::middleware('auth:sanctum')->get('/account', function (Request $request) {
     return $request->user();
-});
+})->name('account');
+
+
+Route::post(
+    '/authorise',
+    [\App\Http\Controllers\LoginController::class, 'authoriseUser'],
+)->name('authorise');
